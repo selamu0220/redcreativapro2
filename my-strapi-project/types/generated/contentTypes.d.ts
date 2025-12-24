244bc191b6c443367b58dd7cd422cd6b689f4b32
selamu0220 <selamu.garciabravo@gmail.com>
2025-12-24 17:38:22 +0100

    Auto-sync after agent response

--- my-strapi-project/types/generated/contentTypes.d.ts
+++ my-strapi-project/types/generated/contentTypes.d.ts
@@ -513,6 +513,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
       ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
     >;
     category: Schema.Attribute.Relation<'manyToOne', 'api::category.category'>;
+    content: Schema.Attribute.RichText;
     cover: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
